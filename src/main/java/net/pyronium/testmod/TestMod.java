0c6e35fc0f3f1fb141ca4b3ea44c4d3420816751
iSchyzm <71853991+iSchyzm@users.noreply.github.com>
2023-02-23 17:43:22 -0500

    added comment

--- src/main/java/net/pyronium/testmod/TestMod.java
+++ src/main/java/net/pyronium/testmod/TestMod.java
@@ -19,6 +19,7 @@ public class TestMod {
     public static final String MOD_ID = "testmod";
     private static final Logger LOGGER = LogUtils.getLogger();
 
+    //important comment
     public TestMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
